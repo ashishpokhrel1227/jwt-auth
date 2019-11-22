48ed450635f08f17b1d377fdc8a3c12495d6f783
ashishpokhrel12271997@gmail.com <ashishpokhrel12271997>
2019-11-22 12:19:22 +0545

    model has been created on note app

--- auth/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Auth
-
-admin.site.register(Auth)
--- note/models.py
+++ note/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Auth(models.Model):
+class Note(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     content = models.TextField()
